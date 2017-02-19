e77666d1d51c24c58b7c7dfe016bec29990ab1a2
Msarker1 <Sarkermt1@gmail.com>
2017-02-19 17:35:21 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Player {
+
+	final int WIDTH = 30 , HEIGHT = 30;
+	
+	public void draw(Graphics g, int xCoor, int yCoor){
+		g.setColor(Color.GRAY);
+		g.fillRect(xCoor, yCoor, WIDTH, HEIGHT);
+		
+	}
+}
