36bd3b772c4f45da6590835f48253cea63239587
Msarker1 <Sarkermt1@gmail.com>
2017-02-19 17:32:56 -0500

    Update Point.java

--- Point.java
+++ Point.java
@@ -6,8 +6,15 @@ public class Point {
 	
 	public void draw(Graphics g, int xCoor, int yCoor){
 		g.setColor(Color.BLACK);
-		g.drawRect(xCoor, yCoor, WIDTH, HEIGHT);
+		g.drawOval(xCoor, yCoor, WIDTH, HEIGHT);
 		g.setColor(Color.YELLOW);
-		g.fillRect(xCoor, yCoor, WIDTH, HEIGHT);
+		g.fillOval(xCoor, yCoor, WIDTH, HEIGHT);
 	}
+	
+	public void delete(Graphics g, int xCoor, int yCoor){
+		g.clearRect(xCoor, yCoor, WIDTH, HEIGHT);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 600, 400);
+	}
+
 }
